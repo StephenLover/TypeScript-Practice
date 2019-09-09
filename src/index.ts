a283e5e71cc174f496f299e3bf55d6ba714ee759
Stephen Wang <paradoxstephen@gmail.com>
2019-09-09 21:39:44 +1000

    adding marker

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { User } from './User';
-import { Company } from './Company';
+// import { Company } from './Company';
+
+import { CustomMap } from './CustomMap';
 
 const user = new User();
+const customMap = new CustomMap('map');
 
-console.log(user)
+customMap.addUserMarker(user);
 
-
-const company = new Company();
-
-console.log(company)
