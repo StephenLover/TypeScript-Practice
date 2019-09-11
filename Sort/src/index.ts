881f9ceddbf38765974f63c958d6371d3b806ef1
Stephen Wang <paradoxstephen@gmail.com>
2019-09-11 21:41:09 +1000

    abstract/interfaces finished

--- Sort/src/index.ts
+++ Sort/src/index.ts
@@ -1,12 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList';
+
 
 const numbersCollection = new NumbersCollection([100, 3, -5, 0])
-const charactersCollection = new CharactersCollection('bXyaa')
+const charactersCollection = new CharactersCollection('bvXyaa')
 
-//const sorter = new Sorter(numbersCollection);
-const sorter = new Sorter(charactersCollection)
-sorter.sort()
-console.log(charactersCollection.data)
+// const sorter = new Sorter(numbersCollection);
+
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+linkedList.sort()
+linkedList.print()
+
+
+
+
 
