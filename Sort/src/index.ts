8be710aeda15959c873be0551cf23048013daa8f
Stephen Wang <paradoxstephen@gmail.com>
2019-09-11 20:38:29 +1000

    sortable interface

--- Sort/src/index.ts
+++ Sort/src/index.ts
@@ -1,9 +1,12 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection'
 
 const numbersCollection = new NumbersCollection([100, 3, -5, 0])
+const charactersCollection = new CharactersCollection('bXyaa')
 
-const sorter = new Sorter(numbersCollection);
+//const sorter = new Sorter(numbersCollection);
+const sorter = new Sorter(charactersCollection)
 sorter.sort()
-console.log(numbersCollection.data)
+console.log(charactersCollection.data)
 
